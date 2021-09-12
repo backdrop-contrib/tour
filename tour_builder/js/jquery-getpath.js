c14a8b49fd1887303fac6f8b9d5ffebe996f4722
docwilmot <docwilmot@users.noreply.github.com>
2021-09-12 14:11:10 -0500

    Merge in tour builder

---
+++ tour_builder/js/jquery-getpath.js
@@ -0,0 +1,43 @@
+/*
+	jQuery-GetPath v0.01, by Dave Cardwell. (2007-04-27)
+	
+	http://davecardwell.co.uk/javascript/jquery/plugins/jquery-getpath/
+	
+	Copyright (c)2007 Dave Cardwell. All rights reserved.
+	Released under the MIT License.
+	
+	
+	Usage:
+	var path = $('#foo').getPath();
+*/
+
+jQuery.fn.extend({
+	getPath: function( path ) {
+		// The first time this function is called, path won't be defined.
+		if ( typeof path == 'undefined' ) path = '';
+
+		// If this element is <body> we've reached the end of the path.
+		if ( this.is('body') )
+			return 'body' + path;
+
+		// Add the element name.
+		var cur = this.get(0).nodeName.toLowerCase();
+
+		// Determine the IDs and path.
+		var id    = this.attr('id');
+		var hclass = this.attr('class');
+
+
+		// Add the #id if there is one.
+		if ( typeof id != 'undefined' ) {
+      return '#' + id + path;
+    }
+
+		// Add any classes.
+		if ( typeof hclass != 'undefined' )
+			cur += '.' + hclass.split(/[\s\n]+/).join('.');
+
+		// Recurse up the DOM.
+		return this.parent().getPath( ' > ' + cur + path );
+	}
+});
---
+++ tour_builder/js/tour_builder.js
@@ -0,0 +1,87 @@
+/* global Backdrop */
+(function ($) { // Avoid leaking variables
+  Backdrop.behaviors.tour_builder = {
+    attach: function (context, settings) {
+      // $(document).on('mouseover', function(e) {
+        // targetElement = $(e.target);
+          // if (targetElement.attr('id')) {
+            // var targetCSS = targetElement.attr('id');
+          // }
+          // else {
+            // var targetCSS = targetElement.getPath();
+          // }
+          // console.log(targetCSS);
+        // targetElement.addClass( "elborder" ).on('click', function() {
+          // console.log(targetElement.getPath());
+        // });
+      // });
+      // $(document).on('mouseout', function(e) {
+        // targetElement = $(e.target);
+        // targetElement.removeClass( "elborder" );
+      // });
+      var targetElement;
+      var targetCSS;
+      $(document).on({
+          mouseover: function (e) {
+            targetElement = $(e.target);
+              if (targetElement.attr('id')) {
+                targetCSS = targetElement.attr('id');
+              }
+              else {
+                targetCSS = targetElement.getPath();
+              }
+            if (validElement()) {
+              console.log(getElementTitle());
+            targetElement.addClass( "elborder" );
+            }
+          },
+          mouseout: function (e) {
+            //targetElement = $(e.target);
+            targetElement.removeClass( "elborder" );
+          },
+          click: function (e) {
+            if (validElement()) {
+              e.preventDefault();
+              targetElement.removeClass( "elborder" );
+              //targetElement = $(e.target);
+              $('input[name="tour_stop_title"]').val(getElementTitle()); 
+              $('textarea[name="tour_stop_css"]').val(targetCSS); 
+              //$('#edit-tour-stop-css').val(targetCSS); 
+            }
+
+              //stuff to do on mouse leave
+          }
+      });
+
+      function validElement () {
+        if (!targetElement.hasClass('tour-edit-link') && 
+            !targetElement.parents('#admin-bar').length &&
+            !targetElement.parents('.tour-edit-dialog').length
+        ) {
+          return true;
+        }
+        return false;
+      }
+
+      function getElementTitle () {
+        if (targetElement.attr('aria-label')) {
+              console.log('hit');
+          return targetElement.attr('aria-label');
+        }
+        if (targetElement.is(':header')) {
+          return targetElement.text();
+        }
+        if (targetElement.is('a, a *')) {
+          return targetElement.text().trim() + ' ' + Backdrop.t('link');
+        }
+        if (targetElement.prop('class')) {
+          return targetElement.prop('class');
+        }
+        return false;
+      }
+
+
+    }
+  };
+
+})(jQuery);
