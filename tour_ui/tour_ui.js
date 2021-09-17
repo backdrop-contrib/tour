2255e24e7a66aab55537ec05dbad4c47474c5992
docwilmot <docwilmot@users.noreply.github.com>
2021-09-17 16:35:14 -0500

    Add Popper popups if available

--- tour_ui/tour_ui.js
+++ tour_ui/tour_ui.js
@@ -1,28 +1,77 @@
 (function ($) {
 
 /**
- * Hides the otional settings textarea in tour_ui_edit_tour().
+ * Add Popper JS Popups to the steps tables.
  *
+ * @todo: revert from new =>{ format to old JS notation.
  */
-Backdrop.behaviors.moduleFilterByText = {
+Backdrop.behaviors.tourPopper = {
   attach: function(context, settings) {
-    var $form = $('#tour-ui-edit-tour');
 
-    $form.find('.requirements').hide();
+    if (typeof(Popper) === 'undefined') {
+      return;
+    }
 
-    // Toggle the requirements info.
-    $('a.requirements-toggle').click(function(e) {
-      var $requirements = $(this).closest('td').find('.requirements').toggle();
-      if ($requirements.is(':visible')) {
-        $(this).text(Backdrop.t('less'));
+    const tooltips = document.querySelectorAll('.tour-ui-table-row');
+
+    tooltips.forEach(tooltip => {
+      const button = tooltip.querySelector('.tour-ui-table-title');
+      const container = tooltip.querySelector('.tour-ui-table-tooltip');
+      
+      const popperInstance = Popper.createPopper(button, container, {
+        placement: 'top',
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 8],
+            },
+          },
+        ],
+      });
+       function show() {
+        // Make the tooltip visible
+        container.setAttribute('data-show', '');
+
+        // Enable the event listeners
+        popperInstance.setOptions((options) => ({
+          ...options,
+          modifiers: [
+            ...options.modifiers,
+            { name: 'eventListeners', enabled: true },
+          ],
+        }));
+
+        // Update its position
+        popperInstance.update();
       }
-      else {
-        $(this).text(Backdrop.t('more'));
+
+      function hide() {
+        // Hide the container
+        container.removeAttribute('data-show');
+
+        // Disable the event listeners
+        popperInstance.setOptions((options) => ({
+          ...options,
+          modifiers: [
+            ...options.modifiers,
+            { name: 'eventListeners', enabled: false },
+          ],
+        }));
       }
-      e.preventDefault();
-      e.stopPropagation();
+
+      const showEvents = ['mouseenter', 'focus'];
+      const hideEvents = ['mouseleave', 'blur'];
+
+      showEvents.forEach((event) => {
+        button.addEventListener(event, show);
+      });
+
+      hideEvents.forEach((event) => {
+        button.addEventListener(event, hide);
+      });
+
     });
-
   }
 };
 
