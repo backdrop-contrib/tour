5e6569a2873a4dd3ddb4835f99f7bf275ec36001
docwilmot <docwilmot@users.noreply.github.com>
2021-10-10 20:28:10 -0500

    Check all plain

--- tour_ui/templates/tour-step-row.tpl.php
+++ tour_ui/templates/tour-step-row.tpl.php
@@ -1,16 +1,16 @@
 <div class="tour-ui-table-row" id="tour-ui-table-row-<?php print $step['id']; ?>">
-  <div class="tour-ui-table-title"><a role="button" tabindex="0"><?php print $step['title']; ?><a>
+  <div class="tour-ui-table-title"><a role="button" tabindex="0"><?php print $step_title; ?><a>
   </div>
 
   <div aria-describedby="step-description" aria-labelledby="step-label" class="tour-ui-table-tooltip shepherd-enabled shepherd-element shepherd-has-cancel-icon shepherd-has-title backdrop-tour" data-shepherd-step-id="step-<?php print $step['id']; ?>" data-popper-placement="top">
     <div class="shepherd-arrow" data-popper-arrow="" ></div> 
     <div class="shepherd-content">
       <header class="shepherd-header">
-        <h3 id="step-label" class="shepherd-title"><?php print $step['title']; ?></h3> 
+        <h3 id="step-label" class="shepherd-title"><?php print $step_title; ?></h3> 
         <button aria-label="Close" class="shepherd-cancel-icon" type="button">
         <span aria-hidden="true">×</span></button>
       </header> 
-      <div class="shepherd-text" id="step-description"><?php print $step['text']; ?></div> 
+      <div class="shepherd-text" id="step-description"><?php print $step_text; ?></div> 
       <footer class="shepherd-footer"></footer>
     </div>
   </div>
